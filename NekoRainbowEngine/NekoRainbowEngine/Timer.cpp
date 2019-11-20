48cb6c044183a4621fab56d6547e1c92d79be19d
Laia <36154539+LaiaMartinezMotis@users.noreply.github.com>
2019-11-20 23:53:03 +0100

    Improved Timer functions

--- NekoRainbowEngine/NekoRainbowEngine/Timer.cpp
+++ NekoRainbowEngine/NekoRainbowEngine/Timer.cpp
@@ -15,17 +15,47 @@ Timer::Timer()
 // ---------------------------------------------
 void Timer::Start()
 {
+	running = true;
 	started_at = SDL_GetTicks();
+}
+
+void Timer::Stop()
+{
+	running = false;
+	stopped_at= SDL_GetTicks();
+}
+
+void Timer::Resume_Timer()
+{
+	if (running == false) 
+	{
+		running = true;
+		started_at = SDL_GetTicks() - (stopped_at - started_at);
+	}
 }
 
 // ---------------------------------------------
 uint32 Timer::Read() const
 {
-	return SDL_GetTicks() - started_at;
+	if (running == true)
+	{
+		return SDL_GetTicks() - started_at;
+	}
+	else
+	{
+		return stopped_at - started_at;
+	}
 }
 
 // ---------------------------------------------
 float Timer::ReadSec() const
 {
-	return float(SDL_GetTicks() - started_at) / 1000.0f;
+	if (running == true) 
+	{
+		return float(SDL_GetTicks() - started_at) / 1000.0f;
+	}
+	else 
+	{
+		return float(stopped_at - started_at) / 1000.0f;
+	}
 }
