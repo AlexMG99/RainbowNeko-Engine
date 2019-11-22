9cc5ac683f640813dbc04d4d956c6a3210c5e907
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2019-11-22 23:43:48 +0100

    Working on Quadtree
    - Adding objects

--- NekoRainbowEngine/NekoRainbowEngine/Quadtree.cpp
+++ NekoRainbowEngine/NekoRainbowEngine/Quadtree.cpp
@@ -2,14 +2,42 @@
 #include "ModuleViewport.h"
 #include "GameObject.h"
 #include "Quadtree.h"
+#include "ComponentMesh.h"
 
 #include "GL/include/glew.h"
 
+Quad::~Quad()
+{
+	if (root != nullptr)
+		RELEASE(root);
+}
+
 void Quad::GenerateQuadtree(AABB section)
 {
+	if (root) {
+		RELEASE(root);
+	}
+
 	root = new QuadNode(section);
 
-	root->SubDivide();
+}
+
+void Quad::Insert(GameObject * obj)
+{
+	ComponentMesh* comp_mesh = obj->GetComponentMesh();
+	if (comp_mesh && comp_mesh->mesh)
+	{
+		if (!root)
+		{
+			GenerateQuadtree(comp_mesh->BB_mesh.GetGlobalAABB(obj));
+		}
+		else if (!IsGameobjectQuad(obj))
+		{
+			quad_objects.push_back(obj);
+			root->Insert(obj, comp_mesh->BB_mesh.GetGlobalAABB(obj));
+		}
+		
+	}
 }
 
 void QuadNode::SubDivide()
@@ -18,12 +46,39 @@ void QuadNode::SubDivide()
 		section.minPoint.y + section.maxPoint.y - section.minPoint.y,
 		section.minPoint.z + (section.maxPoint.z - section.minPoint.z) * 0.5F);
 
-	childrens.push_back(new QuadNode(section.minPoint, mid_point));
-	childrens.push_back(new QuadNode(mid_point, section.maxPoint));
+	AddNode(section.minPoint, mid_point);
+	AddNode(mid_point, section.maxPoint);
 
-	childrens.push_back(new QuadNode(float3(min(mid_point.x, section.maxPoint.x), min(section.minPoint.y, section.maxPoint.y), min(mid_point.z, section.minPoint.z)), float3(max(mid_point.x, section.maxPoint.x), max(section.minPoint.y, section.maxPoint.y), max(mid_point.z, section.minPoint.z))));
-	childrens.push_back(new QuadNode(float3(min(mid_point.x, section.minPoint.x), min(section.minPoint.y, section.maxPoint.y), min(mid_point.z, section.maxPoint.z)), float3(max(mid_point.x, section.minPoint.x), max(section.minPoint.y, section.maxPoint.y), max(mid_point.z, section.maxPoint.z))));
+	AddNode(float3(min(mid_point.x, section.maxPoint.x), min(section.minPoint.y, section.maxPoint.y), min(mid_point.z, section.minPoint.z)), float3(max(mid_point.x, section.maxPoint.x), max(section.minPoint.y, section.maxPoint.y), max(mid_point.z, section.minPoint.z)));
+	AddNode(float3(min(mid_point.x, section.minPoint.x), min(section.minPoint.y, section.maxPoint.y), min(mid_point.z, section.maxPoint.z)), float3(max(mid_point.x, section.minPoint.x), max(section.minPoint.y, section.maxPoint.y), max(mid_point.z, section.maxPoint.z)));
 
+	if (node_objects.size() > BUCKET)
+	{
+
+	}
+}
+
+void QuadNode::AddObjectNode(GameObject * obj)
+{
+
+}
+
+void QuadNode::AddNode(const float3 min, const float3 max)
+{
+	QuadNode* quad_node = new QuadNode(min, max);
+	quad_node->parent = this;
+	childrens.push_back(quad_node);
+}
+
+bool Quad::IsGameobjectQuad(GameObject * obj)
+{
+	bool ret = false;
+
+	if (!quad_objects.empty()) {
+		ret = std::find(quad_objects.begin(), quad_objects.end(), obj) == quad_objects.begin();
+	}
+
+	return ret;
 }
 
 void Quad::Draw()
@@ -43,9 +98,9 @@ QuadNode::QuadNode(const AABB limits)
 	section = limits;
 }
 
-void QuadNode::Insert(GameObject * obj)
+void QuadNode::Insert(GameObject * obj, AABB aabb)
 {
-	
+
 }
 
 void QuadNode::DrawNode()
--- NekoRainbowEngine/NekoRainbowEngine/Quadtree.h
+++ NekoRainbowEngine/NekoRainbowEngine/Quadtree.h
@@ -7,7 +7,7 @@
 
 class GameObject;
 
-#define BUCKET 4
+#define BUCKET 1
 
 class QuadNode {
 public:
@@ -15,15 +15,19 @@ public:
 	QuadNode(const AABB limits);
 	~QuadNode() {};
 
-	void Insert(GameObject* obj);
+	void Insert(GameObject* obj, AABB aabb);
 	void Remove(GameObject* obj) {};
 	void DrawNode();
 
+private:
 	void SubDivide();
+	void AddObjectNode(GameObject* obj);
+	void AddNode(const float3 min, const float3 max);
 
 public:
 	AABB section;
 
+	QuadNode* parent = nullptr;
 	std::vector<GameObject*> node_objects;
 	std::vector<QuadNode*> childrens;
 	
@@ -34,11 +38,13 @@ class Quad
 {
 public:
 	Quad() {};
-	~Quad() {};
+	~Quad();
 
 	void GenerateQuadtree(AABB section);
-	void Insert(GameObject* obj) {};
+	void Insert(GameObject* obj);
 	void Remove(GameObject* obj) {};
+
+	bool IsGameobjectQuad(GameObject* obj);
 
 	void Draw();
 
